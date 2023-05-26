32b7472609a208758dd33cd68fd5d39aebe595c7
Lythium <eduardorodalm95@gmail.com>
2023-05-26 14:50:56 +0100

    finished tic tac toe

--- tic-tac-toe/tests/tic-tac-toe.ts
+++ tic-tac-toe/tests/tic-tac-toe.ts
@@ -1,16 +1,124 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { AnchorError, Program } from '@coral-xyz/anchor';
 import { TicTacToe } from "../target/types/tic_tac_toe";
+import chai from 'chai';
+import { expect } from 'chai';
 
-describe("tic-tac-toe", () => {
+async function play(program: Program<TicTacToe>, game, player,
+  tile, expectedTurn, expectedGameState, expectedBoard) {
+await program.methods
+  .play(tile)
+  .accounts({
+    player: player.publicKey,
+    game
+  })
+  .signers(player instanceof (anchor.Wallet as any) ? [] : [player])
+  .rpc();
+
+const gameState = await program.account.game.fetch(game);
+expect(gameState.turn).to.equal(expectedTurn);
+expect(gameState.state).to.eql(expectedGameState);
+expect(gameState.board)
+  .to
+  .eql(expectedBoard);
+}
+
+
+describe('tic-tac-toe', () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.TicTacToe as Program<TicTacToe>;
+  const programProvider = program.provider as anchor.AnchorProvider;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it('setup game!', async() => {
+    const gameKeypair = anchor.web3.Keypair.generate();
+    const playerOne = programProvider.wallet;
+    const playerTwo = anchor.web3.Keypair.generate();
+    await program.methods
+      .setupGame(playerTwo.publicKey)
+      .accounts({
+        game: gameKeypair.publicKey,
+        playerOne: playerOne.publicKey,
+      })
+      .signers([gameKeypair])
+      .rpc();
+
+    let gameState = await program.account.game.fetch(gameKeypair.publicKey);
+    expect(gameState.turn).to.equal(1);
+    expect(gameState.players)
+      .to
+      .eql([playerOne.publicKey, playerTwo.publicKey]);
+    expect(gameState.state).to.eql({ active: {} });
+    expect(gameState.board)
+      .to
+      .eql([[null,null,null],[null,null,null],[null,null,null]]);
+  });
+
+  it('player one wins!', async () => {
+    const gameKeypair = anchor.web3.Keypair.generate();
+    const playerOne = programProvider.wallet;
+    const playerTwo = anchor.web3.Keypair.generate();
+    await program.methods
+      .setupGame(playerTwo.publicKey)
+      .accounts({
+        game: gameKeypair.publicKey,
+        playerOne: playerOne.publicKey,
+      })
+      .signers([gameKeypair])
+      .rpc();
+
+    let gameState = await program.account.game.fetch(gameKeypair.publicKey);
+    expect(gameState.turn).to.equal(1);
+    expect(gameState.players)
+      .to
+      .eql([playerOne.publicKey, playerTwo.publicKey]);
+    expect(gameState.state).to.eql({ active: {} });
+    expect(gameState.board)
+      .to
+      .eql([[null,null,null],[null,null,null],[null,null,null]]);
+
+    await play(
+      program,
+      gameKeypair.publicKey,
+      playerOne,
+      {row: 0, column: 0},
+      2,
+      { active: {}, },
+      [
+        [{x:{}},null,null],
+        [null,null,null],
+        [null,null,null]
+      ]
+    );
+
+
+    try {
+      await play(
+        program,
+        gameKeypair.publicKey,
+        playerOne, // same player in subsequent turns
+        // change sth about the tx because
+        // duplicate tx that come in too fast
+        // after each other may get dropped
+        {row: 1, column: 0},
+        2,
+        { active: {}, },
+        [
+          [{x:{}},null,null],
+          [null,null,null],
+          [null,null,null]
+        ]
+      );
+      chai.assert(false, "should've failed but didn't ");
+    } catch (_err) {
+      expect(_err).to.be.instanceOf(AnchorError);
+      const err: AnchorError = _err;
+      expect(err.error.errorCode.code).to.equal("NotPlayersTurn");
+      expect(err.error.errorCode.number).to.equal(6004);
+      expect(err.program.equals(program.programId)).is.true;
+      expect(err.error.comparedValues).to.deep.equal([playerTwo.publicKey, playerOne.publicKey]);
+    }
+
   });
 });
